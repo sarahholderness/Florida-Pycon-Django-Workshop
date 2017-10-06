3bb8b1be517ca874b7a3fd4eb7463f4a93aac807
sarah <sarah-holderness@pluralsight.com>
2017-10-06 16:14:11 -0400

    Level 3 changes

--- Treasuregram/main_app/views.py
+++ Treasuregram/main_app/views.py
@@ -1,22 +1,6 @@
 from django.shortcuts import render
-
+from .models import Treasure
 # Create your views here.
 def index(request):
+    treasures = Treasure.objects.all()
     return render(request, 'index.html', {'treasures': treasures})
-
-class Treasure:
-    def __init__(self, name, value, material, location, img_url):
-        self.name = name
-        self.value = value
-        self.material = material
-        self.location = location
-        self.img_url = img_url
-
-treasures = [
-    Treasure('Gold Nugget', 500.00, 'gold', "Curly's Creek, NM",
-    'https://s3.amazonaws.com/courseware.codeschool.com/try_django/images/treasuregram-gold-nugget.png'),
-    Treasure("Fool's Gold", 0.00, 'pyrite', "Fool's Falls, CO",
-    'https://s3.amazonaws.com/courseware.codeschool.com/try_django/images/treasuregram-fools-gold.png'),
-    Treasure('Coffee Can', 20.00, 'tin', 'Acme, CA',
-    'https://s3.amazonaws.com/courseware.codeschool.com/try_django/images/treasuregram-coffee-can.png'),
-]
