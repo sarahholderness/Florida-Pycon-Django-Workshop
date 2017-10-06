ed350b0e6765bb8459dbc993afd4cac00ee2acbd
sarah <sarah-holderness@pluralsight.com>
2017-10-05 22:16:21 -0400

    Corrected styles for Level 2

--- Treasuregram/main_app/views.py
+++ Treasuregram/main_app/views.py
@@ -5,14 +5,18 @@ def index(request):
     return render(request, 'index.html', {'treasures': treasures})
 
 class Treasure:
-    def __init__(self, name, value, material, location):
+    def __init__(self, name, value, material, location, img_url):
         self.name = name
         self.value = value
         self.material = material
         self.location = location
+        self.img_url = img_url
 
 treasures = [
-    Treasure('Gold Nugget', 500.00, 'gold', "Curly's Creek, NM"),
-    Treasure("Fool's Gold", 0.00, 'pyrite', "Fool's Falls, CO"),
-    Treasure('Coffee Can', 20.00, 'tin', 'Acme, CA'),
+    Treasure('Gold Nugget', 500.00, 'gold', "Curly's Creek, NM",
+    'https://s3.amazonaws.com/courseware.codeschool.com/try_django/images/treasuregram-gold-nugget.png'),
+    Treasure("Fool's Gold", 0.00, 'pyrite', "Fool's Falls, CO",
+    'https://s3.amazonaws.com/courseware.codeschool.com/try_django/images/treasuregram-fools-gold.png'),
+    Treasure('Coffee Can', 20.00, 'tin', 'Acme, CA',
+    'https://s3.amazonaws.com/courseware.codeschool.com/try_django/images/treasuregram-coffee-can.png'),
 ]
