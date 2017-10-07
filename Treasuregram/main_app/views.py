9bc19752a46ef47cf205e33ace7771bdde04334c
sarah <sarah-holderness@pluralsight.com>
2017-10-06 20:17:16 -0400

    Level 1 Section 2

--- Treasuregram/main_app/views.py
+++ Treasuregram/main_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from .models import Treasure
+
 # Create your views here.
 def index(request):
     treasures = Treasure.objects.all()
     return render(request, 'index.html', {'treasures': treasures})
+
+def detail(request, treasure_id):
+    treasure = Treasure.objects.get(id=treasure_id)
+    return render(request, 'detail.html', {'treasure': treasure})
