50912eeceaea42c6a4f1eb024a3f0631426846f7
sarah <sarah-holderness@pluralsight.com>
2017-10-06 21:20:07 -0400

    Level 2 - added form

---
+++ Treasuregram/main_app/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Treasure
+
+class TreasureForm(forms.ModelForm):
+    class Meta:
+        model=Treasure
+        fields=['name', 'value', 'location', 'material', 'img_url']
